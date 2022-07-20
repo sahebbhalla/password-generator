d1378013ba8d353252b3bed701993ef79524818f
saheb <sahibbhalla.010@gmail.com>
2022-07-20 19:23:37 -0400

    Added more functions to disect the code into individual functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,9 @@ var specialCharacter=["!","#","$","%","&","(",")","*","+",",","-"];//Array to ea
 var upperCase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];//Array to randomly select a UpperCase Character, More characters can be added if the oragnization decides to give user option
 var number=["0","1","2","3","4","5","6","7","8","9"];//Array to randomly select a numberr, More numbers can be added if the oragnization decides to give user option
 
-//generatePassword Function
-var generatePassword= function(){
-  var pwd = "";//A new empty string for a new password
-  var pwdLength=0;//Length check
+// Function to get Length from the user 
+var getLength =function(pwdLength){
+
   while(pwdLength<8 || pwdLength>128||isNaN(pwdLength)){//Loop until coorect choice is made 
  
     pwdLength = window.prompt("How Long do you want your password to be ");//get length
@@ -17,11 +16,30 @@ var generatePassword= function(){
       window.alert("Password has to be more 8 or less than 128");
     }
   }
- 
-  var specialCharacterQuestion = window.confirm("Do you want to include at least one special character, e.g., ! @ # ? ] ");//Special Character Questions
-  var caseControl = window.confirm("Do you want to include uppercase letters ?");//UpperCase Letter Question
-  var numberControl = window.confirm("Do you want a password with numbers ?");//number question 
-  
+  return pwdLength;
+}
+
+
+//generatePassword Function
+var generatePassword= function(){
+  var pwd = "";//A new empty string for a new password
+  var pwdLength=0;//Length check
+  pwdLength=getLength(pwdLength);
+
+  var atLeastOneSelected = false;//condtion to loop until the user selects one criteria 
+    while(!atLeastOneSelected){
+      var specialCharacterQuestion = window.confirm("Do you want to include at least one special character, e.g., ! @ # ? ] ");//Special Character Questions
+      var caseControl = window.confirm("Do you want to include uppercase letters ?");//UpperCase Letter Question
+      var numberControl = window.confirm("Do you want a password with numbers ?");//number question
+
+      if(specialCharacter==true||caseControl==true||numberControl==true){//if atleast one is selected set the while loop condtion to true 
+        atLeastOneSelected=true;
+      }
+      else{
+        atLeastOneSelected=false;
+        window.alert("You need to sleect atleast One option")//Ask the user to reneter thier choices 
+      }
+    }
   for(var i = 0; i <pwdLength;i++){//Loop for ever password character.
     var internalRandom = Math.floor(Math.random() *4);///Internal random for switch thus creating a unbiased seqeuence of characters
     
